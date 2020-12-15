5aff6147d6cdc6cd631baa2626eae4c9214b394e
Kata Gál <kata.gal@stud.uni-corvinus.hu>
2020-12-15 19:18:08 +0100

    commit chart

--- Project_ar0ez3/Project_ar0ez3/Form3.Designer.cs
+++ Project_ar0ez3/Project_ar0ez3/Form3.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.salesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.salesDatabaseDataSet = new Project_ar0ez3.SalesDatabaseDataSet();
@@ -77,17 +77,17 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(321, 262);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Sales";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Sales";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(781, 364);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
@@ -197,6 +197,7 @@
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
+           // this.Shown += new System.EventHandler(this.Form3_Shown);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.salesBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.salesDatabaseDataSet)).EndInit();
--- Project_ar0ez3/Project_ar0ez3/Form3.cs
+++ Project_ar0ez3/Project_ar0ez3/Form3.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.UI.DataVisualization.Charting;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -19,17 +18,31 @@ namespace Project_ar0ez3
         {
             InitializeComponent();
         }
-
+     
         private void Form3_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'salesDatabaseDataSet.Category' table. You can move, or remove it, as needed.
             this.categoryTableAdapter.Fill(this.salesDatabaseDataSet.Category);
             // TODO: This line of code loads data into the 'salesDatabaseDataSet.Sales' table. You can move, or remove it, as needed.
             this.salesTableAdapter.Fill(this.salesDatabaseDataSet.Sales);
-           // var category = from sales in context.Sales
-                          // join product in context.Products on product.salesPk equals sales.id
-                         //  join category in context.Categories on product.categoryPk equals category.id
-           }
+            // var category = from sales in context.Sales
+            // join product in context.Products on product.salesPk equals sales.id
+            //  join category in context.Categories on product.categoryPk equals category.id
+
+           // chart1.ChartAreas[0].AxisX.Minimum = 0;
+           // chart1.ChartAreas[0].AxisX.Maximum = 2000000000;
+
+         
+
+          //  var year2014 = chart1.Series.Add(“2014”);
+            //var year2015 = chart1.Series.Add(“2015”);
+
+            //year2014.Points.AddXY(“Sales”, Convert.ToDouble(label2014.Text));
+           // year2015.Points.AddXY(“Sales”, Convert.ToDouble(label2015.Text));
+
+            //string[] xYears = { "2014", "2015", "2016", "2017", "2018", "2019" };
+
+        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -78,24 +91,47 @@ namespace Project_ar0ez3
                               where row.Cells[5].FormattedValue.ToString() != string.Empty
                               select Convert.ToInt32(row.Cells[5].FormattedValue)).Sum().ToString();
 
-            var series = chart1.Series[0];
-            series.XValueMember = "Year";
-            series.YValueMembers = "Sales";
-            double[] yValues =                
-            { 
-                Convert.ToDouble(label2014.Text), 
-                Convert.ToDouble(label2015.Text), 
-                Convert.ToDouble(label2016.Text), 
-                Convert.ToDouble(label2017.Text), 
-                Convert.ToDouble(label2018.Text), 
-                Convert.ToDouble(label2019.Text), 
-            };
+           ///var series = chart1.Series[0];
+           //series.XValueMember = "Year";
+           // series.YValueMembers = "Sales";
+           //double[] yValues =                
+           // { 
+           //    Convert.ToDouble(label2014.Text), 
+           //    Convert.ToDouble(label2015.Text), 
+           //    Convert.ToDouble(label2016.Text), 
+           //    Convert.ToDouble(label2017.Text), 
+           //    Convert.ToDouble(label2018.Text), 
+           //    Convert.ToDouble(label2019.Text), 
+           // };
 
-            string[] xYears = { "2014", "2015", "2016", "2017", "2018", "2019" };
-           
-           
+            
         }
 
-        
+        private void Form3_Shown(object sender, EventArgs e)
+        {
+            //chart1.ChartAreas[0].AxisX.Minimum = 2013;
+            //chart1.ChartAreas[0].AxisX.Maximum = 2020;
+
+          
+
+
+            //Series S1 = new Series();
+            //S1.Points.AddXY(Convert.ToDouble(label2014.Text), 2014);
+            //S1.Points.AddXY(Convert.ToDouble(label2015.Text), 2015);
+            //S1.Points.AddXY(Convert.ToDouble(label2016.Text), 2016);
+            //S1.Points.AddXY(Convert.ToDouble(label2017.Text), 2017);
+            //S1.Points.AddXY(Convert.ToDouble(label2018.Text), 2018);
+            //S1.Points.AddXY(Convert.ToDouble(label2019.Text), 2019);
+
+            //chart1.Series[0].Points[0].AxisLabel = "2014";
+            //chart1.Series[0].Points[1].AxisLabel = "2015";
+            //chart1.Series[0].Points[2].AxisLabel = "2016";
+            //chart1.Series[0].Points[2].AxisLabel = "2017";
+            //chart1.Series[0].Points[2].AxisLabel = "2018";
+            //chart1.Series[0].Points[2].AxisLabel = "2019";
+        }
     }
+
+
+
 }
